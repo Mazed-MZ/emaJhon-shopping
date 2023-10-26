24ec96dceab710aee6ca2f58609533c8ccfd36f6
Mazed-MZ <mazedislam21@gmail.com>
2023-10-26 20:34:28 +0600

    getfun

--- src/components/Shop/Order/Order.jsx
+++ src/components/Shop/Order/Order.jsx
@@ -8,18 +8,22 @@ export default function Order({cart}) {
   // const {cart} = props; -->> Option two <<--
 
   let total = 0;
+  let totalShipping = 0;
   for(const product of cart){
     total = total + product.price;
+    totalShipping = totalShipping + product.shipping;
   }
+  const tax = total*7/100;
+  const grandTotal = total + totalShipping + tax;
 
   return (
     <div className='order-cart'>
       <h2>Order summury</h2>
       <h4>Selected Items: {cart.length}</h4>
       <h4>Total Price: ${total}</h4>
-      <h4>Total shipping charge: </h4>
-      <h4>Tax added: </h4>
-      <h4>Grand Total: </h4>
+      <h4>Total shipping charge: ${totalShipping}</h4>
+      <h4>Tax added: ${tax.toFixed(2)}</h4>
+      <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
     </div>
   )
 }
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,19 +2,25 @@ import React, { useEffect, useState } from 'react';
 import Products from './Products/Products';
 import Order from './Order/Order';
 import './Shop.css';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 export default function Shop() {
 
     const [product, setProduct] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(() => {
-        fetch('fakedata.json')
+        fetch(`https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json`)
             .then(res => res.json())
             .then(data => setProduct(data))
     }, [])
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
+    },[])
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
     return (
         <div className='shop-container'>
